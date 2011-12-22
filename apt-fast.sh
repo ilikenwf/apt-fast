39f656aa38ac495cd8cdfc556b356535585519f8
Matt Parnell <parwok@gmail.com>
2011-12-22 14:09:30 -0800

    add old, original apt-fast to repo

--- apt-fast.sh
+++ apt-fast.sh
@@ -1,40 +1,25 @@
-# !/bin/sh
-# apt-fast v0.03 by Matt Parnell http://www.mattparnell.com, GNU GPLv3
-# Use this just like apt-get for faster package downloading.
+#!/bin/sh
+#apt-fast by Matt Parnell http://www.mattparnell.com
+# Use this just like apt-get for faster package downloading. Make sure to have axel installed
 
-# Check for proper priveliges
-[ "`whoami`" = root ] || exec sudo "$0" "$@"
-
-# Test if the axel is installed
-if [ ! -x /usr/bin/axel ]
-then echo "axel is not installed, perform this?(y/n)"
-    read ops
-    case $ops in
-         y) if apt-get install axel -y --force-yes
-               then echo "axel installed"
-            else echo "unable to install the axel. you are using sudo?" ; exit
-            fi ;;
-         n) echo "not possible usage apt-fast" ; exit ;;
-    esac
-fi
-
-# If the user entered arguments contain upgrade, install, or dist-upgrade
-if echo "$@" | grep -q "upgrade\|install\|dist-upgrade"; then
+#If the first user entered variable string contains apt-get, and the second string entered is either install or dist-upgrade
+if echo "$1" | grep -q "[upgrade]" || echo "$2" | grep -q "[install]" || echo "$2" | grep -q "[dist-upgrade]"; then
   echo "Working...";
-
-  # Go into the directory apt-get normally puts downloaded packages
+  #Go into the directory apt-get normally puts downloaded packages
   cd /var/cache/apt/archives/;
+  #Have apt-get print the information, including the URI's to the packages
+  apt-get -y --print-uris $1 $2 $3 $4 > debs.list;
+  #Strip out the URI's, and download the packages with Axel for speediness
+  egrep -o -e "(ht|f)tp://[^\']+" debs.list | xargs -l1 axel -a;
+  #Perform the user's reqested action via apt-get 
+  apt-get -y $1 $2 $3 $4;
 
-  # Have apt-get print the information, including the URI's to the packages
-  # Strip out the URI's, and download the packages with Axel for speediness
-  # I found this regex elsewhere, showing how to manually strip package URI's you may need...thanks to whoever wrote it
-  apt-get -y --print-uris $@ | egrep -o -e "(ht|f)tp://[^\']+" > apt-fast.list && cat apt-fast.list | xargs -l1 axel -a
+  echo "Done! Make sure and check to see that the packages all were installed properly. If a package is erred, run sudo apt-get autoclean and try installing it again without the use of this script.";
 
-  # Perform the user's requested action via apt-get
-  apt-get $@;
-
-  echo -e "\nDone! Verify that all packages were installed successfully. If errors are found, run apt-get clean as root and try again using apt-get directly.\n";
-
+elif echo "$1" | grep -q "[*]"; then
+  apt-get $1;
 else
-   apt-get $@;
+  echo "Sorry, but you appear to be entering invalid options. You must use apt-get and one of apt-get's options in order to use this script.";
 fi
+
+

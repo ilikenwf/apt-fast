007db709c6a1f481be0ed348b48f057ce5062845
changbiao <5.5.5.b.i.a.o@163.com>
2016-12-16 15:45:43 +0800

    Update quick-install.sh
    for me

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y aria2 git
 if ! [[ -f /usr/bin/apt-fast ]]; then
-  git clone https://github.com/ilikenwf/apt-fast /tmp/apt-fast
+  git clone https://github.com/changbiao/apt-fast /tmp/apt-fast
   sudo cp /tmp/apt-fast/apt-fast /usr/bin
   sudo chmod +x /usr/bin/apt-fast
   sudo cp /tmp/apt-fast/apt-fast.conf /etc

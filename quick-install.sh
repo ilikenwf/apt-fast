ca9d8015977e08034f7391ca6b41eec54a92cd7a
Dominique Lasserre <lasserre.d@gmail.com>
2017-11-20 20:46:19 +0100

    quick-install.sh improvements, closes #112
     * Don't run apt-get update/installation if aria is already installed.
     * Allow execution as root (without sudo).

--- quick-install.sh
+++ quick-install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y aria2
-if ! [[ -f /usr/local/sbin/apt-fast ]]; then
-  sudo wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
-  sudo chmod +x /usr/local/sbin/apt-fast
-  sudo wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast.conf -O /etc/apt-fast.conf
+set -e
+
+apt_fast_installation() {
+  if ! dpkg-query --show aria2 >/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y aria2
+  fi
+
+  wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
+  chmod +x /usr/local/sbin/apt-fast
+  if ! [[ -f /etc/apt-fast.conf ]]; then
+    wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast.conf -O /etc/apt-fast.conf
+  fi
+}
+
+
+if [[ "$EUID" -eq 0 ]]; then
+  apt_fast_installation
+else
+  type sudo >/dev/null 2>&1 || { echo "sudo not installed, change into root context" >&2; exit 1; }
+
+  DECL="$(declare -f apt_fast_installation)"
+  sudo bash -c "$DECL; apt_fast_installation"
 fi

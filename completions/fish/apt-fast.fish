3e128c5f424554331c5dbc003613c54cb85d0861
Dominique Lasserre <lasserre.d@gmail.com>
2024-07-28 16:44:59 +0200

    Remove debug output

--- completions/fish/apt-fast.fish
+++ completions/fish/apt-fast.fish
@@ -3,7 +3,6 @@ if [ -f /etc/apt-fast.conf ]
     set completion_cmd (bash -c 'source /etc/apt-fast.conf && echo "$_APTMGR"')
 end
 set completion_cmd (echo "$completion_cmd" | string split ' ' | head -n 1)
-echo $completion_cmd
 if not type -q "$completion_cmd"
     set completion_cmd apt-get
 end

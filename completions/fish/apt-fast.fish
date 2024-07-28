d80e67b9e5a8b72fe06712307b2d2bfb3d6f49bf
Dominique Lasserre <lasserre.d@gmail.com>
2024-07-28 16:41:06 +0200

    Completion: Use completion for configured apt manager Closes #244
     * Fallback to apt-get completion

--- completions/fish/apt-fast.fish
+++ completions/fish/apt-fast.fish
@@ -1,1 +1,11 @@
-complete -c apt-fast -w apt-get
+set completion_cmd apt-get
+if [ -f /etc/apt-fast.conf ]
+    set completion_cmd (bash -c 'source /etc/apt-fast.conf && echo "$_APTMGR"')
+end
+set completion_cmd (echo "$completion_cmd" | string split ' ' | head -n 1)
+echo $completion_cmd
+if not type -q "$completion_cmd"
+    set completion_cmd apt-get
+end
+
+complete -c apt-fast -w "$completion_cmd"
